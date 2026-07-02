3a4850230b475a4f83fd47f49784b26feceea861
Frodo <frodo6frodo@gmail.com>
2026-07-02 14:57:18 +0200

    added manual cropping, OCR changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.frodo.fighttracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.9"
+        versionCode = 10
+        versionName = "1.9.2"
     }
 
     buildTypes {
